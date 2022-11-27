0400bfc2986664969d7c123ca298edb59d4c8806
Donghyun Kim <kdh949@naver.com>
2022-11-27 15:15:41 +0900

    10위 순위 출력으로 되돌리기 더블 버퍼링 없앰

--- Project2/shooting_game.c
+++ Project2/shooting_game.c
@@ -1,112 +1,22 @@
-/* 
+/*
 * 1. 왼쪽 비행기 안닿게(1칸 띄우기)
 * 2. 비행기(l-o-l), 적디자인 (ж)
-* 3. 색상 정하는 것 -비행기 : 흰색, 총알: 민트/하늘색, 테두리: 그대로, 적: 그대로, 
-* 4. 벽면이 움직이고, 적이 깜빡거리고 => 더블 버퍼링 이용 필연적 //쉽지 않을 것으로 보임
+* 3. 색상 정하는 것 -비행기 : 흰색, 총알: 민트/하늘색, 테두리: 그대로, 적: 그대로,
+* 4. 벽면이 움직이고, 적이 깜빡거리고
 * 5. 난이도 (적이 출현하는 속도)
-* 6. 총알속도 조절 설명  (+, - 설명) 
+* 6. 총알속도 조절 설명  (+, - 설명)
 * 7. 종료화면을 화려하게
 * 8. 적한테 부딪혔을때 비행기 색상 빨간색으로 (0.3초 정도) 바뀜
 * 9. 감점기준이 비행기 어디든지 닿았을 때
 * 10. *파일포인터 이용 -> 순위표(점수 높은 순 정렬/등수), 닉네임 입력/10등까지만, score.txt (닉네임 점수\n) //일부완료
-* 11. 계속 하겠는지 확인 
+* 11. 계속 하겠는지 확인
 */
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <windows.h>
+#include <stdlib.h>
 #include <time.h>
-
-#define BX 20 
-#define BY 1  
-#define BW 80 // 게임판의 넓이
-#define BH 26 // 게임판의 높이
-#define UX 50 // 플레이어 초기 위치
-#define UY 12 // 플레이어 초기 위치
-#define MAXBULLET 8
-#define MAXENEMY 10
-
-#define BWF 90 // 게임판의 넓이
-#define BHF 32 // 게임판의 높이
-
-/*===============================*/
-/* 더블 버퍼링 테스트 변수 부분(시작) */
-HANDLE hBuffer[2];  //버퍼 핸들
-int nScreenIndex;   //현재 선택 버퍼가 뭔지 저장
-/* 더블 버퍼링 테스트 변수 부분(끝) */
-/*===============================*/
-
-/*===============================*/
-/* 더블 버퍼링 테스트 함수 부분(시작) */
-/* 1. 버퍼 생성 */
-void CreatBuffer()
-{
-	COORD size = { BWF, BHF };
-	CONSOLE_CURSOR_INFO cci;
-	SMALL_RECT rect;
-	rect.Bottom = 0;
-	rect.Left = 0;
-	rect.Right = BWF;
-	rect.Top = BHF;
-
-	hBuffer[0] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleScreenBufferSize(hBuffer[0], size);
-	SetConsoleWindowInfo(hBuffer[0], TRUE, &rect);
-
-	hBuffer[1] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleScreenBufferSize(hBuffer[1], size);
-	SetConsoleWindowInfo(hBuffer[1], TRUE, &rect);
-
-	cci.dwSize = 1;
-	cci.bVisible = FALSE;
-	SetConsoleCursorInfo(hBuffer[0], &cci);
-	SetConsoleCursorInfo(hBuffer[1], &cci);
-}
-
-/*2. 버퍼 쓰기 x, y, 문자 */
-void WriteBuffer(int x, int y, char str[]) 
-{
-	DWORD dw;
-	COORD CursorPosition = { x, y };
-	SetConsoleCursorPosition(hBuffer[nScreenIndex], CursorPosition);
-	WriteFile(hBuffer[nScreenIndex], str, strlen(str), &dw, NULL);
-}
-
-
-/*3. 버퍼 전환*/
-void FlippingBuffer() 
-{
-	//Sleep(33);  //부드러운 플리핑을 위한 0.033초의 딜레이(사람은 1초에 33장의 연속적인 그림을 볼 때 가장 부드럽게 인식하고 합니다. 그래서 0.033초)
-	SetConsoleActiveScreenBuffer(hBuffer[nScreenIndex]);
-	nScreenIndex = !nScreenIndex;
-}
-
-/* 4. 버퍼 내용 지우기 */
-void ClearBuffer()
-{
-	COORD Coor = { BHF,BWF };
-	DWORD dw;
-	FillConsoleOutputCharacter(hBuffer[!nScreenIndex], ' ', BHF*BWF, Coor, &dw);
-}
-
-/*5. 버퍼해제*/
-void DeleteBuffer() 
-{
-	CloseHandle(hBuffer[0]);
-	CloseHandle(hBuffer[1]);
-
-}
-
-/* [+]버퍼링 전용 색상지정*/
-void SetColor(unsigned short color)
-{
-	SetConsoleTextAttribute(hBuffer[nScreenIndex], color);
-}
-
-/* 더블 버퍼링 테스트 함수 부분(끝) */
-/*===============================*/
-
 
 /* 지연 함수 */
 void delay(int n) {
@@ -118,9 +28,6 @@ void gotoxy(int x, int y) { //gotoxy함수
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-void clear() {
-	system("cls");
-}
 
 /* cmd창 좌표 이동 함수 */
 //void gotoxy(int x, int y) {
@@ -138,6 +45,14 @@ void color(int n) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), n);
 }
 
+#define BX 20 
+#define BY 1  
+#define BW 80 // 게임판의 넓이
+#define BH 26 // 게임판의 높이
+#define UX 50 // 플레이어 초기 위치
+#define UY 12 // 플레이어 초기 위치
+#define MAXBULLET 8
+#define MAXENEMY 10
 
 BOOL IsKeyDown(int Key)
 {
@@ -192,8 +107,6 @@ int enemyframe = 3;
 
 int main()
 {
-	CreatBuffer();
-	
 	player.x = UX;
 	player.y = UY;
 	player.hp = 3;
@@ -204,100 +117,69 @@ int main()
 	srand((unsigned)time(NULL));
 	system("cls");
 
-	//ScoreBoard(1, 1, 2); //test 곧 삭제예정
+	ScoreBoard(1, 1, 2); //test 곧 삭제예정
 
-	//char nick[10];
+	char nick[10];
 
-	//printf("닉네임을 입력해주세요!: ");
-	//scanf("%s", nick);
-	//system("cls");
+	printf("닉네임을 입력해주세요!: ");
+	scanf("%s", nick);
+	system("cls");
 
 	showcursor(0); //커서 숨기기
-	//gotoxy(player.x, player.y);
-	//printf("U★U");
-	
-	WriteBuffer(player.x, player.y, "U★U");
-	
-	
-	int count = 0;
-	while(1) {
-		PrintWall();
-		FlippingBuffer();
-		PrintWall();
-		PrintFloor();
-		/*gotoxy(player.x, player.y);
-		printf("U●U");*/
-		WriteBuffer(player.x, player.y, "U★U");
+	gotoxy(player.x, player.y);
+	printf("U★U");
 
+	PrintWall();
+
+	int count = 0;
+	while (1) {
+		PrintFloor();
+		gotoxy(player.x, player.y);
+		printf("U●U");
 		if (count % 2 == 0)
 		{
 			if (IsKeyDown(VK_LEFT))
 			{
 				if (player.x > BX + 2)
 				{
-					/*gotoxy(player.x, player.y);
-					printf("    ");*/
-					WriteBuffer(player.x, player.y, "    ");
-					FlippingBuffer();
-					WriteBuffer(player.x, player.y, "    ");
+					gotoxy(player.x, player.y);
+					printf("    ");
 					player.x--;
-					/*gotoxy(player.x, player.y);
-					printf("U●U");*/
-					WriteBuffer(player.x, player.y, "U★U");
-					FlippingBuffer();
-					WriteBuffer(player.x, player.y, "U★U");
-					
+					gotoxy(player.x, player.y);
+					printf("U●U");
 				}
 			}
 			if (IsKeyDown(VK_RIGHT))
 			{
 				if (player.x < BW - 5)
 				{
-					/*gotoxy(player.x, player.y);
-					printf("    ");*/
-					WriteBuffer(player.x, player.y, "    ");
-					FlippingBuffer();
-					WriteBuffer(player.x, player.y, "    ");
+					gotoxy(player.x, player.y);
+					printf("    ");
 					player.x++;
-					/*gotoxy(player.x, player.y);
-					printf("U●U");*/
-					WriteBuffer(player.x, player.y, "U★U");
-					FlippingBuffer();
-					WriteBuffer(player.x, player.y, "U★U");
+					gotoxy(player.x, player.y);
+					printf("U●U");
 				}
 			}
 			if (IsKeyDown(VK_UP))
 			{
 				if (player.y > 2)
 				{
-					/*gotoxy(player.x, player.y);
-					printf("    ");*/
-					WriteBuffer(player.x, player.y, "    ");
-					FlippingBuffer();
-					WriteBuffer(player.x, player.y, "    ");
+					gotoxy(player.x, player.y);
+					printf("    ");
 					player.y--;
-					/*gotoxy(player.x, player.y);
-					printf("U●U");*/
-					WriteBuffer(player.x, player.y, "U★U");
-					FlippingBuffer();
-					WriteBuffer(player.x, player.y, "U★U");
+					gotoxy(player.x, player.y);
+					printf("U●U");
 				}
 			}
 			if (IsKeyDown(VK_DOWN))
 			{
 				if (player.y < 25)
 				{
-					/*gotoxy(player.x, player.y);
-					printf("    ");*/
-					WriteBuffer(player.x, player.y, "    ");
-					FlippingBuffer();
-					WriteBuffer(player.x, player.y, "    ");
+					gotoxy(player.x, player.y);
+					printf("    ");
 					player.y++;
-					/*gotoxy(player.x, player.y);
-					printf("U●U");*/
-					WriteBuffer(player.x, player.y, "U★U");
-					FlippingBuffer();
-					WriteBuffer(player.x, player.y, "U★U");
+					gotoxy(player.x, player.y);
+					printf("U●U");
 				}
 			}
 		}
@@ -334,9 +216,8 @@ int main()
 		{
 			CreateEnemy();
 		}
-		if (count % enemyframe == 0) {
+		if (count % enemyframe == 0)
 			MoveEnemy();
-		}
 
 		PlayerHit();
 		MoveBullet();
@@ -345,15 +226,9 @@ int main()
 		score += 1;
 		Sleep(20);
 		count++;
-		//ClearBuffer();
-
 	}
 
-	DeleteBuffer(); //버퍼링 삭제
-
-	//ScoreBoard(nick, score, 1);
-
-
+	ScoreBoard(nick, score, 1);
 	system("cls");
 	gotoxy(UX, UY);
 	printf(" Game Over");
@@ -362,12 +237,9 @@ int main()
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0);
 	Sleep(1000);
 	system("cls");
-
-	//ScoreBoard(nick, score, 2);
-
+	ScoreBoard(nick, score, 2);
 	Sleep(1000);
 	getch();
-	
 
 	return 0;
 }
@@ -492,11 +364,8 @@ void MoveEnemy()
 
 			if (enemy[i].exist == TRUE)
 			{
-				/*gotoxy(enemy[i].x, enemy[i].y);
-				printf("  ");*/
-				WriteBuffer(enemy[i].x, enemy[i].y, "  ");
-				FlippingBuffer();
-				WriteBuffer(enemy[i].x, enemy[i].y, "  ");
+				gotoxy(enemy[i].x, enemy[i].y);
+				printf("  ");
 				switch (enemy[i].direction)
 				{
 				case 1: // ↑
@@ -528,19 +397,10 @@ void MoveEnemy()
 					enemy[i].y -= 1;
 					break;
 				}
-				/*gotoxy(enemy[i].x, enemy[i].y);*/
-				//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
-				/*puts("★");*/
-
-				SetColor(6);
-				WriteBuffer(enemy[i].x, enemy[i].y, "★");
-				SetColor(7);
-				FlippingBuffer();
-				SetColor(6);
-				WriteBuffer(enemy[i].x, enemy[i].y, "★");
-				SetColor(7);
-
-				//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+				gotoxy(enemy[i].x, enemy[i].y);
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
+				puts("★");
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 			}
 		}
 	}
@@ -556,14 +416,12 @@ void Enemyfall()
 		{
 			if (enemy[i].y == pBullet[j].y && abs(enemy[i].x - pBullet[j].x) <= 2)
 			{
-				/*gotoxy(pBullet[j].x, pBullet[j].y);
-				printf("  ");*/
-				WriteBuffer(pBullet[j].x, pBullet[j].y, "  ");
+				gotoxy(pBullet[j].x, pBullet[j].y);
+				printf("  ");
 				enemy[i].exist = FALSE;
 				pBullet[j].exist = FALSE;
-				/*gotoxy(enemy[i].x, enemy[i].y);
-				printf("     ");*/
-				WriteBuffer(enemy[i].x, enemy[i].y, "     ");
+				gotoxy(enemy[i].x, enemy[i].y);
+				printf("     ");
 				score += 10;
 				break;
 			}
@@ -576,21 +434,16 @@ void MoveBullet() {
 	for (int i = 0; i < MAXBULLET; i++)
 	{
 		if (pBullet[i].exist == TRUE) {
-			/*gotoxy(pBullet[i].x, pBullet[i].y);
-			printf("  ");*/
-			WriteBuffer(pBullet[i].x, pBullet[i].y, "  ");
+			gotoxy(pBullet[i].x, pBullet[i].y);
+			printf("  ");
 			pBullet[i].y--;
+			gotoxy(pBullet[i].x, pBullet[i].y);
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
-			/*gotoxy(pBullet[i].x, pBullet[i].y);
-			printf("||");*/
-			SetColor(4);
-			//WriteBuffer(pBullet[i].x, pBullet[i].y, "||");
-			SetColor(7);
-			//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+			printf("||");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 			if (pBullet[i].y <= 1) {
-				/*gotoxy(pBullet[i].x, pBullet[i].y);
-				printf("  ");*/
-				WriteBuffer(pBullet[i].x, pBullet[i].y, "  ");
+				gotoxy(pBullet[i].x, pBullet[i].y);
+				printf("  ");
 				pBullet[i].exist = FALSE;
 			}
 		}
@@ -621,8 +474,7 @@ void TextEnemyFrame(int frame)
 		textframe = 1;
 		break;
 	}
-	//printf("총알 속도 : %d", textframe);
-	//WriteBuffer()
+	printf("총알 속도 : %d", textframe);
 }
 
 void PrintWall() {
@@ -632,23 +484,18 @@ void PrintWall() {
 	//printf("▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩");
 
 	for (int i = BX; i < BW + 1; i++) {
-		//gotoxy(i, 0);
-		WriteBuffer(i, 0, "▩");
-		//printf("▩");
-		
-		//gotoxy(i, 27);
-		WriteBuffer(i, 27, "▩");
-		//printf("▩");
+		gotoxy(i, 0);
+		printf("▩");
+		gotoxy(i, 27);
+		printf("▩");
 	}
 
 	for (int i = BY; i < BH + 1; i++)
 	{
-		WriteBuffer(20, i, "▩");
-		//gotoxy(20, i);
-		//printf("▩");
-		WriteBuffer(80, i, "▩");
-		//gotoxy(80, i);
-		//printf("▩");
+		gotoxy(20, i);
+		printf("▩");
+		gotoxy(80, i);
+		printf("▩");
 	}
 }
 
@@ -698,7 +545,7 @@ void PrintFloor()
 /*스코어보드 함수, 1=점수 쓰기, 2= 점수 읽기*/
 void ScoreBoard(char nick[10], int new_score, int mode) {
 	FILE* fp;
-	
+
 	if (mode == 1) { //쓰기모드
 		fp = fopen("score.txt", "a");
 		fprintf(fp, "%s %d\n", nick, new_score);
@@ -712,7 +559,7 @@ void ScoreBoard(char nick[10], int new_score, int mode) {
 			fprintf(stderr, "오류발생!!");
 			exit(0);
 		}
-		
+
 		/* 데이터를 불러와서 배열에 저장하는 부분 */
 		int n = 0;
 		while (feof(fp) == 0) {
@@ -722,16 +569,16 @@ void ScoreBoard(char nick[10], int new_score, int mode) {
 		fclose(fp);
 
 		/* 값 정렬 후 배열 변경 부분 */
-		for (int i = 0; i < n - 1; i++) 
+		for (int i = 0; i < n - 1; i++)
 		{
-			for (int j = i + 1; j < n; j++) 
+			for (int j = i + 1; j < n; j++)
 			{
-				if (list[i].score < list[j].score) 
+				if (list[i].score < list[j].score)
 				{
 					int_swap(&list[i].score, &list[j].score);
 					char_swap(list[i].nick, list[j].nick);
 				}
-			}	
+			}
 		}
 
 		/* 데이터 출력 부분 */
@@ -742,7 +589,7 @@ void ScoreBoard(char nick[10], int new_score, int mode) {
 		int rank = 0;
 		int recursive = 0;
 		for (int i = 0; i < n; i++) {
-				if (i > 0 && (list[i].score == list[i - 1].score)) { //동점이라면
+			if (i > 0 && (list[i].score == list[i - 1].score)) { //동점이라면
 				printf(" %-6d%-12s%d\n", rank, list[i].nick, list[i].score);
 				recursive++;
 				continue;
@@ -756,10 +603,10 @@ void ScoreBoard(char nick[10], int new_score, int mode) {
 			recursive = 0;
 		}
 	}
-	
-} 
 
-void int_swap(int *a, int *b) {
+}
+
+void int_swap(int* a, int* b) {
 	int tmp;
 	tmp = *a;
 	*a = *b;
@@ -772,4 +619,3 @@ void char_swap(char a[10], char b[10]) {
 	strcpy(a, b);
 	strcpy(b, tmp);
 }
-
